83e20c6b7a17188f16590e0b542edc0b71ca7f06
Ash <ashley.m.crowther+github@gmail.com>
2017-03-27 23:52:32 +0100

    A tiny bit more parsing code.

--- topsy-lib.go
+++ topsy-lib.go
@@ -4,11 +4,17 @@ import "os"
 import "io"
 import "bufio"
 import "regexp"
+import "errors"
+import "fmt"
+
+//TODO::
+var _ = fmt.Printf
 
 type Kind int
 const (
 	KCons = iota
 	KText
+	KSymbol
 )
 type Datum interface {
 	kind() Kind
@@ -18,13 +24,15 @@ type Cons struct {
 	cdr Datum
 }
 type Text string
+type Symbol string
 func (f Cons) kind() Kind { return KCons }
 func (f Text) kind() Kind { return KText }
+func (f Symbol) kind() Kind { return KSymbol }
 
 //Line oriented. Supports partial reads
 type sourceFile struct {
 	//pending is data which has already been read by reader, but pushed back.
-	pending string
+	pending *string
 	reader *bufio.Reader
 	//lineNo and charNo refer to the start of "pending"
 	lineNo int
@@ -35,7 +43,7 @@ type sourceFile struct {
 //Returns nil, nil if there is input but it doesn't match
 //Returns nil, io.EOF at EOF
 func (src *sourceFile) ReadMatch(re *regexp.Regexp) (*string, error) {
-	if(src.pending=="") {
+	if(src.pending==nil) {
 		s, e := src.reader.ReadString('\n')
 		if e!=nil {
 			if e==io.EOF {
@@ -46,39 +54,77 @@ func (src *sourceFile) ReadMatch(re *regexp.Regexp) (*string, error) {
 		}
 		src.lineNo++
 		src.charNo=1
-		src.pending=s
+		src.pending=&s
 	}
-	loc:=re.FindStringIndex(src.pending)
+	loc:=re.FindStringIndex(*src.pending)
 	if loc==nil {
 		//Didn't find
 		return nil, nil
 	}
-	ret:=src.pending[loc[0]:loc[1]]
-	src.pending=src.pending[loc[1]+1:]
+	ret:=(*src.pending)[loc[0]:loc[1]]
+	if loc[1]<len(*src.pending) {
+		*src.pending=(*src.pending)[loc[1]:]
+	} else {
+		src.pending=nil
+	}
 	src.charNo+=loc[1];
 	return &ret, nil
 }
 
-//Returns a datum, and the remaining unmatched input
+var reLeadingToken=regexp.MustCompile(`^([[:space:]]+|"[^"]*"|[()]|[^()[:space:]]+)`)
+var reSpace=regexp.MustCompile(`^[[:space:]]+$`)
+var reString=regexp.MustCompile(`^"[^"]*"$`)
+//parse a file
 func Read(src *sourceFile) (Datum, error) {
+	d, e:=ReadChild(src);
+	switch e {
+		case nil:
+fmt.Println(d);
+			return nil, errors.New("Unexpected termination of Read. Extraneous closing bracket?");
+		case io.EOF:
+			return d, e;
+		default:
+			return nil, e;
+	}
+}
+
+func ReadChild(src *sourceFile) (Datum, error) {
 	var tree Datum=nil;
 	for {
-		var reAll=regexp.MustCompile(`^.*`)
-		s, e:=src.ReadMatch(reAll);
-		if e != nil {
-			if e == io.EOF {
-				return tree, nil
-			}
-			return nil, e
+		s, e:=src.ReadMatch(reLeadingToken);
+		switch e {
+			case nil:
+			case io.EOF:
+				return tree, io.EOF
+			default:
+				return nil, e
 		}
-		tree=Cons{Text(*s), tree}
+		switch {
+		case reSpace.MatchString(*s)://Discard
+		case reString.MatchString(*s):
+			tree=Cons{"TEXT:"+Text((*s)[1:len(*s)-1]), tree}
+		case *s=="(":
+			children, e:=ReadChild(src);
+			switch e {
+			case nil:
+				tree=Cons{children,tree};
+			case io.EOF:
+				return nil, errors.New("Unexpected EOF in Read. Missing closing bracket?");
+			default:
+				return nil, e;
+			}
+		case *s==")":
+			return tree, nil
+		default:
+			tree=Cons{"SYMBOL:"+Symbol(*s), tree}
+		}
 	}
 }
 
 func Lex(f *os.File) (Datum, error) {
 	defer f.Close()
 	reader:=bufio.NewReader(f)
-	src:=&sourceFile{"",reader,1,1}
+	src:=&sourceFile{nil,reader,1,1}
 	return Read(src)
 }
 
